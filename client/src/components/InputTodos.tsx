c62e8a7b0775f2ec02419d1df003eba6b4197d5b
Simran <bhatnagarsimran1306@gmail.com>
2024-09-07 23:09:06 +0530

    beautified the look

--- client/src/components/InputTodos.tsx
+++ client/src/components/InputTodos.tsx
@@ -1,18 +1,18 @@
 function InputTodos() {
   return (
     <>
-      <div className="h-screen flex flex-col bg-green-100">
-        <h1 className="text-4xl font-bold flex justify-center pt-10 pb-2">Listly</h1>
+      <div className="h-screen flex flex-col bg-gradient-to-br from-green-200 to-white">
+        <h1 className="text-5xl font-bold flex justify-center pt-10 pb-2 text-green-500">Listly</h1>
         <form className="flex justify-center mt-12">
           <div className="relative w-4/12">
             <input
               type="text"
               placeholder="Enter your todo"
-              className="outline-none w-full py-3 px-2 rounded-3xl"
+              className="outline-none w-full py-3 px-3 rounded-3xl"
             />
             <button
               type="submit"
-              className="absolute right-1 top-1 bg-green-600 px-3 py-2 rounded-3xl"
+              className="absolute right-1 top-1 bg-green-500 px-3 py-2 rounded-3xl"
             >
               Go
             </button>
